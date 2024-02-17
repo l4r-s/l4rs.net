c2102c8b6a534ab98fcb06cdf1e072d233f03eaa
Lars Bättig <me@l4rs.net>
2024-02-17 16:16:52 +0100

    2nd example card

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -12,7 +12,7 @@ const Posts: React.FC<Props> = () => {
         <h1 className="text-4xl uppercase text-center">Latest bits</h1>
         <div className="divider divide-gray-400 mb-20"></div>
 
-        <div className="card card-side bg-base-100 shadow-xl">
+        <div className="card card-side bg-base-100 shadow-xl hover:shadow-2xl cursor-pointer my-5">
           <figure>
             <img
               src="https://media-exp1.licdn.com/dms/image/D5612AQFG4GGVZ1-LlQ/article-cover_image-shrink_600_2000/0/1667324699780?e=2147483647&v=beta&t=MERZD-vIso8L-dS2am_KbCo9dr4yiLIfSA4K_csxepo"
@@ -28,6 +28,25 @@ const Posts: React.FC<Props> = () => {
             <div className="card-actions text-gray-300 mt-5">Feb 17 2024</div>
           </div>
         </div>
+
+        <div className="card card-side bg-base-100 shadow-xl hover:shadow-2xl cursor-pointer my-5">
+          <figure>
+            <img
+              src="https://media-exp1.licdn.com/dms/image/D5612AQFG4GGVZ1-LlQ/article-cover_image-shrink_600_2000/0/1667324699780?e=2147483647&v=beta&t=MERZD-vIso8L-dS2am_KbCo9dr4yiLIfSA4K_csxepo"
+              alt="GCP Cloud run"
+              className="w-40 md:w-60 lg:w-80 overflow-hidden"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">GCP Cloud Run deployment with Python</h2>
+            <p>
+              How I manage my Cloud run deployments in every region with Python!
+            </p>
+            <div className="card-actions text-gray-300 mt-5">Feb 17 2024</div>
+          </div>
+        </div>
+
+
       </div>
     </div>
   );
