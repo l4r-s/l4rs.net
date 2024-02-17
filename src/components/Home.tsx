49024385e9f15c7a1bd1dbd74658ef54b94b4fed
Lars Bättig <me@l4rs.net>
2024-02-17 14:09:36 +0100

    fix bg

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -25,19 +25,17 @@ const Home: React.FC<Props> = (props) => {
     return () => clearTimeout(timeoutId);
   }, []);
 
-  const bgUrl = "/bg1.webp";
-
   return (
     <div>
       <div
-        className={`relative hero min-h-screen bg-[url('${bgUrl}')] overflow-hidden bg-cover bg-center backdrop-blur-sm backdrop-filter ${
+        className={`relative hero min-h-screen bg-[url('/bg1.webp')] overflow-hidden bg-cover bg-center backdrop-blur-sm backdrop-filter ${
           isBounce ? "animate-bounce" : ""
         }`}
       >
         <div className="absolute inset-0 backdrop-filter backdrop-blur-lg"></div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full overflow-hidden border-white border-2 flex items-center justify-center w-96 h-96 md:w-120 md:h-120 lg:w-150 lg:h-150">
           <div
-            className={`bg-[url('${bgUrl}')] bg-center bg-cover w-full h-full flex items-center justify-center`}
+            className={`bg-[url('/bg1.webp')] bg-center bg-cover w-full h-full flex items-center justify-center`}
           >
             <h1 className="text-white text-6xl md:text-7xl lg:text-9xl">
               <p className="underline pb-5">Hi!</p>
