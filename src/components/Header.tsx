493d18e93348170d0bbde298e9f878f94c698924
Lars Bättig <me@l4rs.net>
2024-02-18 22:07:34 +0100

    fixes drawer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -74,76 +74,89 @@ const Header: React.FC<Props> = () => {
             className="drawer-overlay"
           ></label>
 
-          <ul className="menu min-h-full w-80 gap-2 bg-base-100 p-4 text-base-content">
-            <li className="font-bold text-center text-xl flex items-center">
-              <a href="/">
-                <Avatar />
-              </a>
-            </li>
-            <div className="text-center mt-3 prose prose-sm">
-              <p>
-                <b className="text-primary prose-2xl">Hi!</b>, my name is Lars.
-              </p>
-              <p>
-                I am a Software and Systems Engineer located in Switzerland.
-              </p>
-              <p className="mt-3 font-thin italic">
-                <b className="text-primary">#</b>opensource{" "}
-                <b className="text-primary">#</b>observability{" "}
-                <b className="text-primary">#</b>ansible{" "}
-                <b className="text-primary">#</b>python{" "}
-                <b className="text-primary">#</b>prometheus{" "}
-                <b className="text-primary">#</b>elk
-              </p>
-            </div>
-            <div className="flex-grow"></div>
-
-            <li className="font-medium">
-              <a href="#">
-                <IconStars />
-                <span className="underline decoration-primary">Projects</span>
-              </a>
-            </li>
-            <li className="font-medium">
-              <a href="#">
-                <IconSticker2 />
-                <span className="underline decoration-primary">Posts</span>
-              </a>
-            </li>
-            <li className="font-medium">
-              <a href="#features">
-                <IconAddressBook />
-                <span className="underline decoration-primary">Contact</span>
-              </a>
-            </li>
-            <div className="divider divide-gray-400"></div>
-
-            <div className="flex self-center -mt-4">
-              <div className="tooltip tooltip-top" data-tip="Github">
-                <div className="btn mx-2">
-                  <a href="https://github.com/l4r-s" target="_blank">
-                    <FaGithub color="black" size={25} />
-                  </a>
-                </div>
+          <div className="min-h-full w-80 gap-2 bg-base-100 p-4 text-base-content">
+            <div className="flex-grow">
+              <div className="font-bold text-center text-xl self-center">
+                <a href="/">
+                  <Avatar />
+                </a>
               </div>
-
-              <div className="tooltip tooltip-top" data-tip="Twitter / X">
-                <div className="btn mx-2">
-                  <a href="https://twitter.com/_l4rs" target="_blank">
-                    <FaTwitter color="black" size={25} />
-                  </a>
-                </div>
-              </div>
-
-              <div className="tooltip tooltip-top" data-tip="Matrix chat">
-                <div className="btn mx-2">
-                  <a href="https://matrix.to/#/@me:l4rs.net" target="_blank">
-                    <IconBrandMatrix color="black" size={25} />
-                  </a>
-                </div>
+              <div className="text-center mt-3 prose prose-sm">
+                <p>
+                  <b className="text-primary prose-2xl">Hi!</b>, my name is
+                  Lars.
+                </p>
+                <p>
+                  I am a Software and Systems Engineer located in Switzerland.
+                </p>
+                <p className="mt-3 font-thin italic">
+                  <b className="text-primary">#</b>opensource{" "}
+                  <b className="text-primary">#</b>observability{" "}
+                  <b className="text-primary">#</b>ansible{" "}
+                  <b className="text-primary">#</b>python{" "}
+                  <b className="text-primary">#</b>prometheus{" "}
+                  <b className="text-primary">#</b>elk
+                </p>
               </div>
             </div>
-          </ul>
+
+            <div className="fixed bottom-2">
+              <ul className="menu">
+                <li className="font-medium">
+                  <a href="#">
+                    <IconStars />
+                    <span className="underline decoration-primary">
+                      Projects
+                    </span>
+                  </a>
+                </li>
+                <li className="font-medium">
+                  <a href="#">
+                    <IconSticker2 />
+                    <span className="underline decoration-primary">Posts</span>
+                  </a>
+                </li>
+                <li className="font-medium">
+                  <a href="#features">
+                    <IconAddressBook />
+                    <span className="underline decoration-primary">
+                      Contact
+                    </span>
+                  </a>
+                </li>
+                <div className="divider divide-gray-400"></div>
+
+                <div className="flex self-center">
+                  <div className="tooltip tooltip-top" data-tip="Github">
+                    <div className="btn mx-2">
+                      <a href="https://github.com/l4r-s" target="_blank">
+                        <FaGithub color="black" size={25} />
+                      </a>
+                    </div>
+                  </div>
+
+                  <div className="tooltip tooltip-top" data-tip="Twitter / X">
+                    <div className="btn mx-2">
+                      <a href="https://twitter.com/_l4rs" target="_blank">
+                        <FaTwitter color="black" size={25} />
+                      </a>
+                    </div>
+                  </div>
+
+                  <div className="tooltip tooltip-top" data-tip="Matrix chat">
+                    <div className="btn mx-2">
+                      <a
+                        href="https://matrix.to/#/@me:l4rs.net"
+                        target="_blank"
+                      >
+                        <IconBrandMatrix color="black" size={25} />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
