e2d4665541d32a49eb52ace2dadc36ae1c23dc31
Lars Bättig <me@l4rs.net>
2024-03-05 21:46:55 +0100

    remove start element after rendering

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root")!;
+const startElement = document.getElementById("start");
+
+ReactDOM.createRoot(rootElement).render(<App />);
+
+if (startElement) {
+  startElement.remove();
+}
