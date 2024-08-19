4af13877035fa24b40a17642e0a6c60a1224341b
Lars Bättig <me@l4rs.net>
2024-08-19 15:43:25 +0200

    improve referrrer

--- v2/app/v2e/page.tsx
+++ v2/app/v2e/page.tsx
@@ -41,9 +41,20 @@ export const metadata = {
   },
 };
 
-export default function Page() {
+export default function Page({ searchParams }: { searchParams: { [key: string]: string } }) {
   const reqHeaders = headers();
+
+  // get referrer from headers this is the url of the page that the user was on before they were redirected to this page
   const referrer = reqHeaders.get('referer') || 'https://hilars.dev/v2e';
-  const url = `https://builditn0w.gumroad.com/l/chrome-manifest-v2-phaseout/BUILD?referrer=${encodeURIComponent(referrer)}`;
+
+  // get referrer from search params (?r=myreferrer), this overrides the referrer from headers if it exists
+  let customReferrer = searchParams.r || referrer;
+
+  // Gumroad referrer must start with http:// or https:// but can be anything
+  if (!customReferrer.startsWith('http://') && !customReferrer.startsWith('https://')) {
+    customReferrer = `https://${customReferrer}`;
+  }
+
+  const url = `https://builditn0w.gumroad.com/l/chrome-manifest-v2-phaseout/BUILD?referrer=${encodeURIComponent(customReferrer)}`;
   redirect(url);
 }
