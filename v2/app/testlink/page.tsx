68c2de0953521096da090cd298cd4a40d590c0ac
Lars Bättig <me@l4rs.net>
2024-08-19 20:26:22 +0200

    test

--- v2/app/testlink/page.tsx
+++ v2/app/testlink/page.tsx
@@ -1,8 +1,8 @@
 import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 
-const title = "5,000+ Opportunities Waiting for You!";
-const description = "As of June 3, 2024, Chrome officially deprecated Extension Manifest V2, meaning thousands of extensions will soon stop working.";
+const title = "TEST LINK";
+const description = "TEST LINK";
 
 export const metadata = {
   title,
@@ -10,7 +10,7 @@ export const metadata = {
   author: "Lars",
   date: "2024-08-17",
   url: "https://hilars.dev/v2e",
-  image: "/chrome_v2_extension_og_v2.png",
+  image: "/og.png",
   tags: ["Chrome Extensions", "Manifest V3", "Web Development", "Google Chrome"],
   openGraph: {
     type: "article",
@@ -19,7 +19,7 @@ export const metadata = {
     url: "https://hilars.dev/v2e",
     images: [
       {
-        url: "/chrome_v2_extension_og_v2.png",
+        url: "/og.png",
         width: 1200,
         height: 630,
         alt: title,
@@ -32,7 +32,7 @@ export const metadata = {
     description,
     images: [
       {
-        url: "/chrome_v2_extension_og_v2.png",
+        url: "/og.png",
         width: 1200,
         height: 630,
         alt: title,
@@ -58,3 +58,4 @@ export default function Page({ searchParams }: { searchParams: { [key: string]: 
   const url = `https://builditn0w.gumroad.com/l/chrome-manifest-v2-phaseout/BUILD?referrer=${encodeURIComponent(customReferrer)}`;
   redirect(url);
 }
+
