d20dcc152f501b994872e042da067152651c8969
Lars Bättig <me@l4rs.net>
2024-08-19 15:13:12 +0200

    og update + refeerer

--- v2/app/v2e/page.tsx
+++ v2/app/v2e/page.tsx
@@ -1,7 +1,7 @@
 import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 
-const title = "Unlock Profitable Opportunities: 5,000+ Chrome V2 Extensions Ready for Rebuild!";
+const title = "5,000+ Opportunities Waiting for You!";
 const description = "As of June 3, 2024, Chrome officially deprecated Extension Manifest V2, meaning thousands of extensions will soon stop working.";
 
 export const metadata = {
@@ -10,7 +10,7 @@ export const metadata = {
   author: "Lars",
   date: "2024-08-17",
   url: "https://hilars.dev/v2e",
-  image: "/blog_v2_extensions.webp",
+  image: "/chrome_v2_extension_og_v2.png",
   tags: ["Chrome Extensions", "Manifest V3", "Web Development", "Google Chrome"],
   openGraph: {
     type: "article",
@@ -19,7 +19,7 @@ export const metadata = {
     url: "https://hilars.dev/v2e",
     images: [
       {
-        url: "/blog_v2_extensions.webp",
+        url: "/chrome_v2_extension_og_v2.png",
         width: 1200,
         height: 630,
         alt: title,
@@ -32,7 +32,7 @@ export const metadata = {
     description,
     images: [
       {
-        url: "/blog_v2_extensions.webp",
+        url: "/chrome_v2_extension_og_v2.png",
         width: 1200,
         height: 630,
         alt: title,
@@ -43,7 +43,7 @@ export const metadata = {
 
 export default function Page() {
   const reqHeaders = headers();
-  const referrer = reqHeaders.get('referer') || '';
+  const referrer = reqHeaders.get('referer') || 'https://hilars.dev/v2e';
   const url = `https://builditn0w.gumroad.com/l/chrome-manifest-v2-phaseout/BUILD?referrer=${encodeURIComponent(referrer)}`;
   redirect(url);
 }
