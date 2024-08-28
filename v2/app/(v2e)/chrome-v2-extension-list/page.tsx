fe93ca90d7df37c2d7a3b91f95b062bd0de9f520
Lars Bättig <me@l4rs.net>
2024-08-28 14:19:36 +0200

    use BUILD discount

--- v2/app/(v2e)/chrome-v2-extension-list/page.tsx
+++ v2/app/(v2e)/chrome-v2-extension-list/page.tsx
@@ -16,7 +16,7 @@ export default function Page({ searchParams }: { searchParams: { [key: string]: 
   }
 
   // Get discount code from search params
-  let discountCode = searchParams.d || 'BUILD70';
+  let discountCode = searchParams.d || 'BUILD';
 
   // Ensure the discount exists in the price table, if not use 'BUILD' as default
   const priceTable = {
@@ -25,7 +25,7 @@ export default function Page({ searchParams }: { searchParams: { [key: string]: 
     'AGENCY70': { price: '$9', discount: '70% OFF' },
   };
   if (!priceTable[discountCode as keyof typeof priceTable]) {
-    discountCode = 'BUILD70';
+    discountCode = 'BUILD';
   }
 
   const url = `https://builditn0w.gumroad.com/l/chrome-manifest-v2-phaseout/${discountCode}?referrer=${encodeURIComponent(customReferrer)}`;
