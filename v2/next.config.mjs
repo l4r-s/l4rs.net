52121d8d96aaac7d39b001639b5a0f0fdb81d5f2
Lars Bättig <me@l4rs.net>
2024-08-28 14:58:34 +0200

    alias

--- v2/next.config.mjs
+++ v2/next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
         source: '/v2e',  // The alias URL
         destination: '/chrome-v2-extension-list',  // The actual page
       },
+       {
+        source: '/v2e1',  // The alias URL
+        destination: '/chrome-v2-extension-list',  // The actual page
+      },
       {
         source: '/c2e',  // The alias URL used on twitter bio
         destination: '/chrome-v2-extension-list',  // The actual page
