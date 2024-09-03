ae992ae4ee62b4dd98d76d1317193284037e6736
Lars Bättig <me@l4rs.net>
2024-09-03 20:43:57 +0200

    cleanup

--- app/(v2e)/layout.tsx
+++ app/(v2e)/layout.tsx
@@ -2,7 +2,6 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import "../globals.css";
 import UmamiScript from "../_components/umami";
-import ConfettiComponent from "../_components/confett";
 
 export default function V2eLayout({
   children,
