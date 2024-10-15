07f870428aec06a69af233ad8f6daeac8cd86076
Lars Bättig <me@l4rs.net>
2024-10-15 22:01:48 +0200

    prepare for mdx

---
+++ next.config.js
@@ -0,0 +1,39 @@
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+  output: 'standalone',
+  experimental: {
+    mdxRs: true,
+  },
+  async rewrites() {
+    return [
+        {
+          source: '/v2e',  // The alias URL
+          destination: '/chrome-v2-extension-list',  // The actual page
+        },
+        {
+          source: '/v2el',  // The alias URL
+          destination: '/chrome-v2-extension-list-product',  // The actual page
+        },
+        {
+          source: '/v2e1',  // The alias URL
+          destination: '/chrome-v2-extension-list',  // The actual page
+        },
+        {
+          source: '/c2e',  // The alias URL used on twitter bio
+          destination: '/chrome-v2-extension-list',  // The actual page
+        },
+        {
+          source: '/cv2',  // The alias URL used on twitter bio
+          destination: '/chrome-v2-extension-list',  // The actual page
+        },
+        {
+          source: '/blog/how-to-create-custom-short-links-with-referrer-tracking-using-next-js',
+          destination: '/blog/how-to-create-custom-short-links-with-referrer-tracking-using-nextjs',
+        },
+      ]
+  },
+};
+
+// const withMDX = require('@next/mdx')();
+// module.exports = withMDX(nextConfig);
+module.exports = nextConfig;
